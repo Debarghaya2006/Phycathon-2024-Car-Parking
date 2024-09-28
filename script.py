523c1bb5c5a02fd8b3bf206016c1f3bfe9e36a9c
Debarghaya Mitra <175645893+Debarghaya2006@users.noreply.github.com>
2024-09-28 12:27:19 -0700

    commit

--- script.py
+++ script.py
@@ -8,7 +8,7 @@ GPIO.setup(17, GPIO.OUT, initial=GPIO.LOW)
 def send_command_to_arduino():
     # Set GPIO 17 high (3.3V) to send a signal to Arduino
     GPIO.output(17, GPIO.HIGH)
-    time.sleep(1.0)  # hold the signal for 100ms
+    time.sleep(0.5)  # hold the signal for 100ms
     GPIO.output(17, GPIO.LOW)  # set GPIO 17 low (0V) to reset the signal
 
 # Call the function to send the command
