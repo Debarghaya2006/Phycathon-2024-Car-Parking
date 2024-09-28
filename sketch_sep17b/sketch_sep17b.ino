6bab8d5bdfd3b37f1afabb4bd5c37bbe39d48d40
dipto-fullstack <diptomoydas20@gmail.com>
2024-09-28 11:12:18 +0530

    update code

--- sketch_sep17b/sketch_sep17b.ino
+++ sketch_sep17b/sketch_sep17b.ino
@@ -38,24 +38,24 @@ void loop(){
   }
 
   // Car exiting detection
-  if (digitalRead(IR2) == LOW && flag2 == 0) {
+  if (digitalRead(IR2) == LOW && flag2 == 0 && Slot >= 0 && Slot < 4) {
     flag2 = 1;
     myservo.write(0);  // Open the gate for exiting car
     Serial.println("Car detected at exit. Gate opening...");
-    delay(3000);       // Give the car enough time to exit (5 seconds)
-    Slot++;            // Increase the slot count
+    delay(500);       // Give the car enough time to exit (5 seconds)
+    Slot++;            // Increase the slot count only if there are cars in the lot
   }
 
   // Close the gate after the car has passed
   if (flag1 == 1 || flag2 == 1) {
-    delay(1000);           // Additional delay to ensure the car has fully passed (3 seconds)
+    delay(500);           // Additional delay to ensure the car has fully passed (3 seconds)
     myservo.write(100);     // Close the gate
     Serial.println("Gate closing...");
     
     // Reset the flags only after the gate has been closed
     flag1 = 0;
     flag2 = 0;
-    delay(1000);  // Small delay to prevent immediate re-triggering
+    delay(500);  // Small delay to prevent immediate re-triggering
   }
 
   // Display slot count on the serial monitor
